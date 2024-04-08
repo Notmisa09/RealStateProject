2797ab6bdd1212cb8c6fff2323502738a7aed807
Notmisa09 <125936102+Notmisa09@users.noreply.github.com>
2024-04-08 14:34:49 -0400

    LastChanges

--- RealStateApp.Core.Application/Mapper/GenrealProfile.cs
+++ RealStateApp.Core.Application/Mapper/GenrealProfile.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using RealStateApp.Core.Application.Dto.Acccount.AuthenticateDtos;
 using RealStateApp.Core.Application.Dto.Acccount.ForgotPassword;
 using RealStateApp.Core.Application.Dto.Acccount.Register;
 using RealStateApp.Core.Application.Dto.Acccount.ResetPassword;
@@ -11,11 +12,16 @@ public class GenrealProfile : Profile
     public GenrealProfile()
     {
         #region MyRegion
+        CreateMap<LoginViewModel, AuthenticationRequest>()
+            .ReverseMap()
+            .ForMember(l => l.Error, opt => opt.Ignore())
+            .ForMember(l => l.HasError, opt => opt.Ignore());
+
         CreateMap<RegisterRequest, SaveUserViewModel>()
             .ForMember(r => r.Error, opt => opt.Ignore())
             .ForMember(r => r.HasError, opt => opt.Ignore())
             .ReverseMap();
-        
+
         CreateMap<ForgotPasswordRequest, ForgotPasswordViewModel>()
             .ForMember(r => r.Error, opt => opt.Ignore())
             .ForMember(r => r.HasError, opt => opt.Ignore())
--- RealStateApp.Core.Domain/Entities/SellingType.cs
+++ RealStateApp.Core.Domain/Entities/SellingType.cs
@@ -6,6 +6,7 @@ namespace RealStateApp.Core.Domain.Entities
     {
         public string SellingTypeName { get; set; }
         public string Description {  get; set; }
+        public int? PropertiesAmount { get; set; }
         public ICollection<Properties>? Properties { get; set; }
     }
 }
