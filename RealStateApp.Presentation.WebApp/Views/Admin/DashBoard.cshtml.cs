01c7b4ee009aa4060b11941442470a9daa74634a
Notmisa09 <125936102+Notmisa09@users.noreply.github.com>
2024-04-13 02:08:46 -0400

    DashBoard and some view changes

---
+++ RealStateApp.Presentation.WebApp/Views/Admin/DashBoard.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RealStateApp.Presentation.WebApp.Views.Admin;
+
+public class DashBoard : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ RealStateApp.Presentation.WebApp/Views/Agent/PropertyList.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RealStateApp.Presentation.WebApp.Views.Agent;
+
+public class PropertyList : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
